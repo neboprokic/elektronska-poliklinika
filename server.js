e69a7658cece3a829248bc752da99864b0fc4342
Nebojsa Prokic <neboprokic@gmail.com>
2019-10-19 17:18:19 +0200

    server appointments api

--- server.js
+++ server.js
@@ -58,6 +58,102 @@ app.use((req, res, next) => {
 
 db.defaults(seeds).write();
 
+app.get('/appointments', (req, res) => {
+  const { from, to } = req.query;
+  const appointments = db
+    .get('appointments')
+    .filter(({ date }) => (!from || date >= from) && (!to || date <= to))
+    .value();
+
+  res.send(appointments);
+});
+
+app.post('/appointments', (req, res) => {
+  const me = getMe(req);
+  const appointment = {
+    ...req.body,
+    id: generateUid(),
+    patient: me.role === 'patient' ? me.id : req.body.patient,
+  };
+  const { date, specialist } = req.body;
+  const userEntry = db.get('users').find(user => user.id === specialist);
+  const user = userEntry.value();
+
+  if (!user) return res.status(404).end();
+
+  userEntry
+    .assign({
+      appointments: {
+        ...user.appointments,
+        [date]: (user.appointments[date] || []).concat(appointment.id),
+      },
+    })
+    .write();
+
+  db.get('appointments')
+    .push(appointment)
+    .write();
+
+  res.status(201).send(appointment);
+});
+
+app.post('/appointments/:id', (req, res) => {
+  const { date } = req.body;
+  const appointmentEntry = db.get('appointments').find({ id: req.params.id });
+  const appointment = appointmentEntry.value();
+  const me = getMe(req);
+
+  if (
+    !appointment ||
+    (me.role === 'patient' && appointment.patient !== me.id)
+  ) {
+    return res.status(404).end();
+  }
+
+  if (appointment.date !== date) {
+    const userEntry = db
+      .get('users')
+      .find(user => user.id === appointment.specialist);
+    const user = userEntry.value();
+
+    if (!user) return res.status(404).end();
+
+    userEntry
+      .assign({
+        appointments: {
+          ...user.appointments,
+          [date]: (user.appointments[date] || []).concat(appointment.id),
+          [appointment.date]: user.appointments[appointment.date].filter(
+            id => id !== appointment.id,
+          ),
+        },
+      })
+      .write();
+  }
+
+  appointmentEntry.assign({ ...req.body, id: appointment.id }).write();
+  res.send(appointmentEntry.value());
+});
+
+app.delete('/appointments/:id', (req, res) => {
+  const appointmentEntry = db.get('appointments').find({ id: req.params.id });
+  const appointment = appointmentEntry.value();
+  const me = getMe(req);
+
+  if (
+    !appointment ||
+    (me.role === 'patient' && appointment.patient !== me.id)
+  ) {
+    return res.status(404).end();
+  }
+
+  appointmentEntry
+    .assign({ status: 'cancelled', comment: req.body.comment })
+    .write();
+
+  res.status(204).end();
+});
+
 app.get('/medicaments', (req, res) => {
   res.send(db.get('medicaments').value());
 });
