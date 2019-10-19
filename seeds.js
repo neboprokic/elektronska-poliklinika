e60afdcd8327b38148440015c2733ff717caf931
Nebojsa Prokic <neboprokic@gmail.com>
2019-10-19 17:09:18 +0200

    db seeds

---
+++ seeds.js
@@ -0,0 +1,11 @@
+const seeds = {
+  users: [
+    { username: 'admin', password: 'pass', role: 'admin' },
+  ],
+  appointments: [],
+  medicaments: [],
+  services: [],
+  expenses: [],
+};
+
+module.exports = seeds;
